eb6820440ad359299828e7683e9e78cbed98af1b
Daniel <daniel@192.168.1.8>
2020-11-09 13:19:23 +0300

    first commit

---
+++ routes/api.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Models\Article;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group(['middleware' => ['cors', 'json.response']], function () {
+    // ...
+    Route::post('/login', 'Auth\ApiAuthController@login')->name('login.api');
+    Route::post('/register','App\Http\Controllers\Auth\ApiAuthController@register')->name('register.api');
+    Route::post('/logout', 'Auth\ApiAuthController@logout')->name('logout.api');
+
+
+});
+Route::middleware('auth:api')->group(function () {
+    // our routes to be protected will go in here
+    Route::post('/logout', 'Auth\ApiAuthController@logout')->name('logout.api');
+});
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+
+});
+Route::get('articles', 'App\Http\Controllers\ArticleController@index');
+Route::get('articles/{id}', 'App\Http\Controllers\ArticleController@show');
+Route::post('articles', 'ArticleController@store');
+Route::put('articles/{id}', 'ArticleController@update');
+Route::delete('articles/{id}', 'ArticleController@delete');
